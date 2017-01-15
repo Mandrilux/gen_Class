58304907190d7c15e5ac313d280e5374390b63fc
baptiste heraud <baptiste.heraud@epitech.eu>
2017-01-15 17:47:46 +0100

    test

--- Creator.h
+++ Creator.h
@@ -5,7 +5,7 @@
 ** Login   <root@epitech.net>
 **
 ** Started on  Sun Jan 15 17:22:09 2017 DESKTOP-0S05REL
-** Last update Sun Jan 15 17:32:24 2017 DESKTOP-0S05REL
+** Last update Sun Jan 15 17:45:55 2017 DESKTOP-0S05REL
 */
 
 #ifndef __CREATOR_H__ /* si la constante __FICHIER_H__ n'est pas defini entrer dans l'espace */
@@ -13,12 +13,14 @@
 
 /* corps du fichier .h */
 
+#include <string>
+
 class Creator
 {
  public:
   Creator();
   ~Creator();
-  void createFile();
+  void create(std::string );
 };
 
 #endif /* __CREATOR_H__ */
--- Main.cpp
+++ Main.cpp
@@ -5,23 +5,31 @@
 // Login   <root@epitech.net>
 //
 // Started on  Sun Jan 15 17:18:30 2017 DESKTOP-0S05REL
-// Last update Sun Jan 15 17:32:08 2017 DESKTOP-0S05REL
+// Last update Sun Jan 15 17:47:41 2017 DESKTOP-0S05REL
 //
+
+
+/**** Version 0.1 *****/
 
 #include <iostream>
 #include "Creator.h"
 using namespace std;
 
-void	Creator::createFile()
-{
+Creator::Creator(){}
 
+Creator::~Creator(){}
+
+void	Creator::create(string name_class)
+{
+  (void) name_class;
 }
 
 int main(int argc, char **argv)
 {
-  if (argc < 2)
-    cerr << "Name Class is require for create file " << endl;
-  (void) argv;
-  Creator classe();
-  return (0);
+if (argc < 2)
+  cerr << "Name Class is require for create file " << endl;
+ Creator classe();
+ (void) argv;
+ classe.create(string("lol"));
+return (0);
 }
